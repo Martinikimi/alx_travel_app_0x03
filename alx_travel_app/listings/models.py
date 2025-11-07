a1279bd9bdc8970abfc8986c57989b88ddf15c0f
Martin Kimani <martinikimi7@gmail.com>
2025-11-07 22:57:07 +0300

    payment intergration

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+
+from django.db import models
 from django.contrib.auth.models import User
+
 
 class Listing(models.Model):
     title = models.CharField(max_length=200)
@@ -8,8 +11,10 @@ class Listing(models.Model):
     location = models.CharField(max_length=200)
     available = models.BooleanField(default=True)
 
+
     def __str__(self):
         return self.title
+
 
 class Booking(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -19,8 +24,10 @@ class Booking(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20, default='pending')
 
+
     def __str__(self):
         return f"Booking {self.id}"
+
 
 class Payment(models.Model):
     STATUS_CHOICES = [
@@ -28,13 +35,13 @@ class Payment(models.Model):
         ('completed', 'Completed'),
         ('failed', 'Failed'),
     ]
-    
+   
     booking = models.OneToOneField(Booking, on_delete=models.CASCADE)
     transaction_id = models.CharField(max_length=100, blank=True, null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     chapa_reference = models.CharField(max_length=100, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+   
     def __str__(self):
         return f"Payment {self.id} - {self.status}"
